257c18822ebbcb69de3731c7f5ea1a9382452c96
Mamontov <mamontov.dp@gmail.com>
2017-02-23 12:58:32 +0300

    Добавил заглушку для теста библиотеки

---
+++ iterations/lib-agar-gdx/test/unittest/CollisionTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unittest;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Max Satu
+ */
+public class CollisionTest {
+    
+    public CollisionTest() {
+    }
+
+
+    @Test
+    public void testCollision() {
+    
+    }
+}
